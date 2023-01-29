8aedf6ca8765ad93010058a68311d6161a053790
Maria Caroline Pereira Silva <carol-silva-@hotmail.com>
2023-01-29 11:25:29 -0300

    ---

--- labefy2023/src/components/cadastrarPlay.jsx
+++ labefy2023/src/components/cadastrarPlay.jsx
@@ -1,7 +1,59 @@
+import './App.css';
+import React from "react"
+import {useState} from "react";
+import styled from "styled-components";
+import axios from "axios"
+import { baseUrl, axiosConfig } from "../../constants";
+
+const PlaylistCreationFormContainer = styled.div`
+    height: 500px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const PlaylistCreationForm = styled.form`   
+    height: 100px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-around;
+`
+
+
 export default function CadastrarPlay(){
+    const [inputNameValue, setInputNameValue] = useState("")
+
+    const changeInputNameValue = (e) => {
+        setInputNameValue([...inputNameValue, e.target.value])
+    }
+    const createPlaylist = (e) => {
+        e.preventDefault();
+        const body = {
+            name: setInputNameValue(inputNameValue)
+        };
+        axios.post(baseUrl, body, axiosConfig).then(()=>{
+            alert("Playlist cadastrada com sucesso!")
+        }).catch(error =>{
+            console.log(error)
+        });
+        setInputNameValue({inputNameValue: ""});
+    }
     return(
     <>
-        <h1>CadastrarPlay</h1> 
+        <PlaylistCreationFormContainer>
+            <h2>Cadastrar nova Playlist</h2>
+            <PlaylistCreationForm>
+                <label>Nova playlist</label>
+                <input 
+                placeholder="Nome da Playlist"
+                type="text"
+                value={inputNameValue}
+                onChange={changeInputNameValue}
+                />
+                <button type="submit">Cadastrar Playlist</button>
+            </PlaylistCreationForm>
+        </PlaylistCreationFormContainer>
     </>
     )
 }
--- labefy2023/src/components/visualizarPlaylist.jsx
+++ labefy2023/src/components/visualizarPlaylist.jsx
@@ -1,4 +1,53 @@
+import React from "react";
+import styled from "styled-components";
+import axios from "axios";
+import TrackCard from "../TrackCard/TrackCard";
+import { baseUrl, axiosConfig } from "../../constants";
+import {useState} from "react"
+
+const PlaylistDetailContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+const TrackCreationForm = styled.form`
+    width: 100vw;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+
+    div {
+        display: flex;
+        flex-direction: column;
+    }
+`
+
 export default function VisualizarPlay(){
+    const [tracks, setTracks] = useState([]);
+    const [trackName, setTrackName] = useState("");
+    const [artist, setArtist] = useState("");
+    const [url, setUrl] = useState("");
+
+    componentDidMount = () => {
+        fetchTracks()
+    }
+    const fetchTracks = () => {
+        axios.get(`${baseUrl}/${props.playlistId}/tracks`,axiosConfig).then(response =>{
+            setTracks([tracks, response.data.results])
+        }).catch(error =>{
+            alert(error)
+        })
+    }
+    const removeTrackFromPlaylist = (trackId) => {
+        axios.delete(`${baseUrl}/${this.props.playlistId}/tracks/${trackId}`, axiosConfig).then(() => {
+            this.fetchTracks();
+        }).catch(err => {
+            console.log(err);
+        });
+    };
+    
     return(
     <>
         <h1>VisualizarPlay</h1> 
